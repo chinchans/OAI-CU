79b86106d8be3a4f50f3396816ab86c686ecdfb0
chinchans <vangalachandu77@gmail.com>
2026-04-10 10:03:15 +0000

    Fixed CMake errors

--- openair2/F1AP/MESSAGES/ASN1/f1ap-18.6.0.cmake
+++ openair2/F1AP/MESSAGES/ASN1/f1ap-18.6.0.cmake
@@ -639,6 +639,9 @@ F1AP_LTMCFRAResourceConfig.c
 F1AP_LTMCellSwitchInformation.c
 F1AP_LTMCells-ToBeReleased-Item.c
 F1AP_LTMCells-ToBeReleased-List.c
+F1AP_LTMConfigurationIDMappingList.c
+F1AP_LTMConfiguration.c
+F1AP_LTMIndicator.c
 F1AP_LTMInformation-Modify.c
 F1AP_LTMInformation-Setup.c
 F1AP_LTML2ResetConfiguration-Item.c
